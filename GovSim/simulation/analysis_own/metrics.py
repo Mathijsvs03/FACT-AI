a273712f069ba743c0495acc97f0ee43d3b8515b
ales1708 <alessio1708@gmail.com>
2025-01-17 13:24:06 +0100

    Formatted metric results + metric calculation

---
+++ GovSim/simulation/analysis_own/metrics.py
@@ -0,0 +1,208 @@
+import json
+import os
+import numpy as np
+
+def gain(data):
+    gains = {}
+
+    for persona in data:
+        gains[persona] = 0
+        for event in data[persona]:
+            if event["action"] == "harvesting":
+                gains[persona] += event["resource_collected"]
+
+    return gains
+
+def round_gain(data):
+    round_gains = {}
+
+    for event_round in data:
+        data_round = data[event_round]
+        total_round_gain = 0
+
+        for event in data_round:
+            if event["action"] == "harvesting":
+                total_round_gain += event["resource_collected"]
+
+        round_gains[event_round] = total_round_gain
+
+    return round_gains
+
+def survival_time(data):
+    persona_0 = data["persona_0"]
+    survival_time = 0
+
+    for event in persona_0:
+        event_round = event["round"]
+        if event_round > survival_time:
+            survival_time = event_round
+
+    return survival_time + 1
+
+def survival_rate(survival_times):
+    fails = 0
+    successes = 0
+
+    for time in survival_times:
+        if time == 11:
+            successes += 1
+        else:
+            fails += 1
+
+    return successes / (successes + fails)
+
+def efficiency(data):
+    total_time = survival_time(data)
+
+    initial_resources = data["persona_0"][0]["resource_in_pool_before_harvesting"]
+    initial_threshold = (initial_resources // 2)
+
+    total_gains = sum(gain(data).values())
+    optimal_gains = total_time * initial_threshold
+
+    efficiency = 1 - (max(0, optimal_gains - total_gains) / optimal_gains)
+    return efficiency * 100
+
+def equality(data):
+    n = len(data)
+
+    individual_gains = list(gain(data).values())
+    total_gains = sum(individual_gains)
+
+    pairwise_diff_sum = np.sum([np.abs(individual_gains[i] - individual_gains[j]) for i in range(n) for j in range(n)])
+
+    # Equality formula
+    equality = 1 - (pairwise_diff_sum / (2 * n * total_gains))
+
+    return equality * 100
+
+def over_usage(data):
+    over_usage_events = 0
+    total_events = (survival_time(data)) * len(data)
+
+    for persona in data:
+        for event in data[persona]:
+            if event["action"] == "harvesting":
+                available_resources = event["resource_in_pool_before_harvesting"]
+                threshold = (available_resources // 2) // len(data)
+
+                if event["resource_collected"] > threshold:
+                    over_usage_events += 1
+
+    return (over_usage_events / total_events) * 100
+
+def group_by_persona(data):
+    persona_data = {}
+
+    for event in data:
+        persona = event["agent_id"]
+        if persona == "framework":
+            continue
+
+        if persona not in persona_data:
+            persona_data[persona] = []
+
+        persona_data[persona].append(event)
+
+    return persona_data
+
+def group_by_round(data):
+    round_data = {}
+
+    for event in data:
+        event_round = event["round"]
+
+        if event["agent_id"] == "framework":
+            continue
+
+        if event_round not in round_data:
+            round_data[event_round] = []
+
+        round_data[event_round].append(event)
+
+    return round_data
+
+def write_group_runs(group, experiment):
+    base_path = "../results/"
+
+    path = os.path.join(base_path, experiment, group)
+    runs = os.listdir(path)
+
+    run_data = {}
+
+    gains_values = []
+    survival_times = []
+    efficiency_values = []
+    equality_values = []
+    over_usage_values = []
+
+    for run in runs:
+        run_data[run] = {}
+
+        with open(os.path.join(path, run, "log_env.json"), 'r') as file:
+            data = json.load(file)
+
+
+        agent_data = group_by_persona(data)
+        round_data = group_by_round(data)
+
+        gains = gain(agent_data)
+        gains_values.append(np.mean(list(gains.values())))
+
+        survival_times.append(survival_time(agent_data))
+
+        efficiency_value = efficiency(agent_data)
+        efficiency_values.append(efficiency_value)
+
+        equality_value = equality(agent_data)
+        equality_values.append(equality_value)
+
+        over_usage_value = over_usage(agent_data)
+        over_usage_values.append(over_usage_value)
+
+        run_data[run]["gains"] = gains
+        run_data[run]["survival_time"] = survival_time(agent_data)
+        run_data[run]["efficiency"] = efficiency_value
+        run_data[run]["equality"] = equality_value
+        run_data[run]["over_usage"] = over_usage_value
+
+    survival_rate_value = survival_rate(survival_times)
+
+    run_data["general"] = {}
+    run_data["general"]["survival_rate"] = survival_rate_value
+
+    run_data["general"]["mean_survival"] = np.mean(survival_times)
+    run_data["general"]["std_survival"] = np.std(survival_times)
+
+    run_data["general"]["mean_gains"] = np.mean(gains_values)
+    run_data["general"]["std_gains"] = np.std(gains_values)
+
+    run_data["general"]["mean_efficiency"] = np.mean(efficiency_values)
+    run_data["general"]["std_efficiency"] = np.std(efficiency_values)
+
+    run_data["general"]["mean_equality"] = np.mean(equality_values)
+    run_data["general"]["std_equality"] = np.std(equality_values)
+
+    run_data["general"]["mean_over_usage"] = np.mean(over_usage_values)
+    run_data["general"]["std_over_usage"] = np.std(over_usage_values)
+
+    if "universalization" in group:
+        experiment_route = f"{experiment}/universalization"
+    else:
+        experiment_route = f"{experiment}/baseline"
+
+    with open(f"results_json/{experiment_route}/{group}.json", 'w') as file:
+        json.dump(run_data, file, indent=4)
+
+if __name__ == '__main__':
+    experiments = [["fishing_v6.4", "fish"], ["sheep_v6.4", "sheep"], ["pollution_v6.4", "pollution"]]
+    models = ["Meta-Llama-3-8B-Instruct", "Llama-2-7b-chat-hf", "Llama-2-13b-chat-hf", "Mistral-7b-Instruct-v0.2"]
+
+    tests = ["baseline_concurrent_universalization", "baseline_concurrent",]
+
+
+    for experiment, task in experiments:
+        for model in models:
+            for test in tests:
+                group = f"{model}_{task}_{test}"
+                write_group_runs(group, experiment)
